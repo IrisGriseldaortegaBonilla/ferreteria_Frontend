0daeade9d484a2e314f0409e7b547edc220eb56e
IrisGriseldaortegaBonilla <irisortega799@gmail.com>
2025-06-04 20:14:07 -0600

    validación de inicio de sesión y protección de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Inicio from "./views/Inicio";
@@ -13,34 +12,35 @@ import Usuarios from "./views/Usuarios";
 import CatalogoProductos from "./views/CatalogoProductos";
 import Estadisticas from "./views/Estadisticas";
 import Dashboard from "./views/Dashboard";
+import RutaProtegida from "./rutas/RutaProtegida";
+import PiePagina from "./components/infopie/PiePagina"; 
 import './App.css';
-
 
 const App = () => {
   return (
     <Router>
-      <main className="margen-superior-main">
-
+      <div className="app-wrapper">
         <Encabezado />
 
-        <Routes>
+        <main className="margen-superior-main content">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/inicio" element={<RutaProtegida vista={<Inicio />} />} />
+            <Route path="/clientes" element={<RutaProtegida vista={<Clientes />} />} />
+            <Route path="/empleados" element={<RutaProtegida vista={<Empleados />} />} />
+            <Route path="/productos" element={<RutaProtegida vista={<Productos />} />} />
+            <Route path="/categorias" element={<RutaProtegida vista={<Categorias />} />} />
+            <Route path="/ventas" element={<RutaProtegida vista={<Ventas />} />} />
+            <Route path="/compras" element={<RutaProtegida vista={<Compras />} />} />
+            <Route path="/usuarios" element={<RutaProtegida vista={<Usuarios />} />} />
+            <Route path="/catalogo" element={<RutaProtegida vista={<CatalogoProductos />} />} />
+            <Route path="/estadisticas" element={<RutaProtegida vista={<Estadisticas />} />} />
+            <Route path="/dashboard" element={<RutaProtegida vista={<Dashboard />} />} />
+          </Routes>
 
-          <Route path="/" element={<Login />} />
-          <Route path="/inicio" element={<Inicio/>} />
-          <Route path="/clientes" element={<Clientes/>} />
-          <Route path="/empleados" element={<Empleados/>} />
-          <Route path="/productos" element={<Productos/>} />
-          <Route path="/categorias" element={<Categorias/>} />
-          <Route path="/ventas" element={<Ventas/>} />
-          <Route path="/compras" element={<Compras/>} />
-          <Route path="/usuarios" element={<Usuarios/>} />
-          <Route path="/catalogo" element={<CatalogoProductos/>} />
-          <Route path="/estadisticas" element={<Estadisticas/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          
-
-        </Routes>
-      </main>
+          <PiePagina />
+        </main>
+      </div>
     </Router>
   );
 };
--- src/components/encabezado/Encabezado.jsx
+++ src/components/encabezado/Encabezado.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Container, Nav, Navbar, Offcanvas,NavDropdown } from "react-bootstrap";
-import logo from "/vite.svg"; // Importación del logo de la ferretería
+import logo from "../../assets/Logoblanco.png"; // Importación del logo de la ferretería
 import "bootstrap-icons/font/bootstrap-icons.css"; // Importación de íconos de Bootstrap
 import "../../App.css"; // Estilos personalizados de la aplicación
 
@@ -46,7 +46,7 @@ const Encabezado = () => {
           style={{ cursor: "pointer" }}
         >
           <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />{" "}
-          <strong>◢◤Ferreteria Bangtan◢◤</strong>
+          <strong>Ferreteria Bangtan◢◤</strong>
         </Navbar.Brand>
 
         {/* Botón para alternar el menú lateral en pantallas pequeñas */}
@@ -75,8 +75,14 @@ const Encabezado = () => {
 
           {/* Cuerpo del menú lateral */}
           <Offcanvas.Body>
+
             {/* Navegación */}
             <Nav className="justify-content-end flex-grow-1 pe-3">
+
+             {estaLogueado  ? (
+                <>
+                {/*Opciones visibles solo si el usuario esta logueado*/}
+
               
               {/* Opción de navegación a Inicio */}
               <Nav.Link
@@ -122,9 +128,6 @@ const Encabezado = () => {
               </NavDropdown.Item>
 
             </NavDropdown>
-
-
-
 
               <NavDropdown 
               title={
@@ -198,10 +201,22 @@ const Encabezado = () => {
                 <strong>Dashboard</strong>
               </Nav.Link>
 
+                </>
+             )  :(
+               // Opción visible solo si el usuario no está logueado
+                <Nav.Link
+                  onClick={() => navegarA("/")}
+                  className={estaColapsado ? "text-black" : "text-white"}
+                >
+                  <i className="bi-box-arrow-in-right me-2"></i>
+                  <strong>Iniciar Sesión</strong>
+                </Nav.Link>
+              )}
+
               {/* Lógica condicional para mostrar Cerrar Sesión o Iniciar Sesión */}
               {estaLogueado ? (
                 // Opción de cerrar sesión
-                <Nav.Link
+                                <Nav.Link
                   onClick={cerrarSesion}
                   className={estaColapsado ? "text-black" : "text-white"}
                 >
@@ -209,13 +224,7 @@ const Encabezado = () => {
                 </Nav.Link>
               ) : (
                 ubicacion.pathname === "/" && (
-                  // Opción de iniciar sesión (solo en la ruta raíz)
-                  <Nav.Link
-                    onClick={() => navegarA("/")}
-                    className={estaColapsado ? "text-black" : "text-white"}
-                  >
-                    Iniciar Sesión
-                  </Nav.Link>
+                  <br />
                 )
               )}
             </Nav>
